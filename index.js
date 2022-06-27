4c57d56701bc930117225c04b28b5d38dba58a02
manMurilo <86134210+manMurilo@users.noreply.github.com>
2022-06-27 20:38:26 -0300

    segundo commit

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
 
+const port = 3000
+
 app.get('/',(request, response) => {
-  return response({message: 'Teste'})
+  return response.send(200)
 })
 
-app.listen(3000)
+app.listen(port, () => {
+  console.log(`Servidor Rodando na Porta ${port}`)
+})
